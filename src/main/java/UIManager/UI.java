5f05a0423dc0ee06f4c8a51d9896396ec4cbe6cc
Wang Boqian <e1408896@u.nus.edu>
2025-09-02 11:47:35 +0800

    Add UI.java for generating responses

--- src/main/java/UIManager/UI.java
+++ src/main/java/UIManager/UI.java
@@ -1,4 +1,30 @@
 package UIManager;
 
+import TextTypes.Text;
+import TextTypes.EndText;
+import TextTypes.StartText;
+
 public class UI {
+
+    public UI() {
+        onStart();
+    }
+
+    public void printLine() {
+        System.out.println("________________________________________________________");
+    }
+
+    private void generateResponse(Text text) {
+        System.out.println(text.getText());
+        printLine();
+    }
+
+    private void onStart() {
+        System.out.println(new StartText().getText());
+        printLine();
+    }
+
+    public void onEnd() {
+        System.out.println(new EndText().getText());
+    }
 }
