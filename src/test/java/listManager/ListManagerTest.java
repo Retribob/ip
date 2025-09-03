db01eee17831d86874fcea9e3940cb9c5b3c8885
Wang Boqian <e1408896@u.nus.edu>
2025-09-03 23:27:41 +0800

    Expand JUnit test coverage

--- src/test/java/listManager/ListManagerTest.java
+++ src/test/java/listManager/ListManagerTest.java
@@ -5,6 +5,7 @@ import customExceptions.NoSuchTaskException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ListManagerTest {
     @Test
@@ -23,4 +24,23 @@ public class ListManagerTest {
                         listManager.taskClassifier("deadline Deadline Task /by 2022-01-01").getTaskWithStatus());
     }
 
+    @Test
+    public void taskClassifier_eventTaskDescriptor_eventTask()
+            throws IncompleteTaskException, NoSuchTaskException{
+        ListManager listManager = new ListManager();
+        assertEquals(new Event("event Event Task /from 2022-01-01 /to 2022-01-01").getTaskWithStatus(),
+                listManager.taskClassifier("event Event Task /from 2022-01-01 /to 2022-01-01").getTaskWithStatus());
+    }
+
+    @Test
+    public void taskClassifier_randomString_NoSuchTaskException() {
+        ListManager listManager = new ListManager();
+        try {
+            listManager.taskClassifier("randomString");
+            fail();
+        } catch (Exception e) {
+            assertEquals("Sorry I don't recognize this task, can you please use the keywords?",
+                    e.getMessage());
+        }
+    }
 }
--- src/test/java/listManager/TodoTest.java
+++ src/test/java/listManager/TodoTest.java
@@ -13,6 +13,14 @@ public class TodoTest {
     }
 
     @Test
+    public void getTaskWithStatus_completeStatus_outputCompleteTodoTask() throws IncompleteTaskException {
+        String expectedTask = "[T][X] Todo Task";
+        Task todoTask = new Todo("todo Todo Task");
+        todoTask.changeStatus(true);
+        assertEquals(expectedTask, todoTask.getTaskWithStatus());
+    }
+
+    @Test
     public void toStringFormat_correctTaskDescriptor_returnTaskString() throws IncompleteTaskException{
         String expectedName = "Todo,todo Todo Task,false";
         assertEquals(expectedName, new Todo("todo Todo Task").toStringFormat());
